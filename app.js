633e5c09da1540aba159ae642159e643c4605d72
harsh2002hd <107150302+harsh2002hd@users.noreply.github.com>
2022-11-21 00:54:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 80;
+
+// EXPRESS SPECIFIC STUFF
+app.use(express.urlencoded({ extended: true }))
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {'title': 'HARSH GYM', "content": con}
+    res.status(200).render('index.pug', params);
+})
+
+app.post('/', (req, res)=>{
+    names = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+
+    let outputToWrite = `the name of the client is ${names}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}`
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
+
+})
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
+
---
+++ static/index.js
@@ -0,0 +1,1 @@
+console.log("harsh is a legend");
